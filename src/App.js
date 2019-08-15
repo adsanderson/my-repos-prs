ac5ed7583bf8d344a627b5301a034746263a7d94
Adam Sanderson <adam@adamsanderson.co.uk>
2019-08-15 18:45:08 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { GraphQLClient } from "graphql-request";
+
+const client = new GraphQLClient("https://api.github.com/graphql", {
+  headers: {
+    authorization: `Bearer 82bd2f1f57dd5114b705b25d4aaa81666883f98b`
+  }
+});
+
+function App() {
+  return (
+    <div className="App">
+      <Test />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+function Test() {
+  // const { loading, error, data } = useQuery(gql`
+  //   query {
+  //     viewer {
+  //       repositories(first: 100) {
+  //         nodes {
+  //           name
+  //         }
+  //       }
+  //     }
+  //   }
+  // `);
+
+  useEffect(() => {
+    async function x() {
+      const result = await client.request(`
+    query {
+      viewer {
+        repositories(first: 100) {
+          nodes {
+            name
+          }
+        }
+      }
+    }
+  `);
+      console.log(result);
+    }
+    x();
+  });
+
+  return <div>done</div>;
+}
